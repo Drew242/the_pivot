314cd5280c5a45df3e40935a9883adc6c8620a9f
mitchashby16 <mitchashby16@gmail.com>
2015-08-05 13:58:19 -0600

    formatted tables with bootstrap

--- spec/features/visitor_can_view_items_spec.rb
+++ spec/features/visitor_can_view_items_spec.rb
@@ -8,14 +8,13 @@ RSpec.feature "VisitorCanViewItems", type: :feature do
                   price: 3000,
                   image: "https://s-media-cache-ak0.pinimg.com/236x/90/eb/32/90eb32bc73e010067b15e08cac3ff016.jpg")
     end
-    
+
     it "can view all existing items" do
       visit items_path
 
       expect(page).to have_content("All Items")
       expect(page).to have_content("Parka")
-      expect(page).to have_content("Stay warm in the tundra")
-      expect(page).to have_content("$3000")
+      expect(page).to have_content("$3,000.00")
       expect(page).to have_css("img[src*='https://s-media-cache-ak0.pinimg.com/236x/90/eb/32/90eb32bc73e010067b15e08cac3ff016.jpg']")
     end
   end
