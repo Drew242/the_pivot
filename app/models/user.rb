597a8548cfc4be9e871327f183d4a621444ec348
Drew Conly <atconly@gmail.com>
2015-08-05 15:29:05 -0600

    unit tests passing need to add flash messages and styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_presence_of :username, :password
+  validates_uniqueness_of :username 
 
   def to_param
     "dashboard"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "creates a user with valid params" do
+    user = User.create(valid_params)
+    expect(user).to be_valid
+  end
+
+  it "will not create a user without valid params" do
+    user = User.create({username: "Drew"})
+    expect(user).to_not be_valid
+  end
+
+  it "has a unique username" do
+    User.create(valid_params)
+    user = User.create(valid_params)
+    expect(user).to_not be_valid 
+  end
+
+  private
+
+  def valid_params
+    {username: "Drew", password: "password"}
+  end
+
 end
