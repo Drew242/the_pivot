5dd590b74d8531fb828f0c2da9d9562d6f5ebc82
mitchashby16 <mitchashby16@gmail.com>
2015-08-06 20:53:10 -0600

    fixed item generator bug by deleting the item generator and using a scope to grab the top 3 items

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @items = RandomItemsGenerator.generate(Item.all)
+    if Item.top.size >= 3
+      @items = Item.top
+    else
+      @items = Item.all
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :category
+  scope :top, -> {all.limit(10)}
 
   def self.random_items
     RandomItemsGenerator.generate(all)
