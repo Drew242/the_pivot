0d5ecc591d0408492ecf22f9c3ac635214862d77
Marla Brizel <marlabrizel@users.noreply.github.com>
2015-08-08 11:52:23 -0600

    Update user_must_login_to_checkout_spec.rb

--- spec/features/user_must_login_to_checkout_spec.rb
+++ spec/features/user_must_login_to_checkout_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 RSpec.feature "UserMustLoginToCheckout", type: :feature do
   context "when logged out" do
-
     before do
       @item = Item.create(title: "hat", description: "a nice hat", price: 3000)
     end
