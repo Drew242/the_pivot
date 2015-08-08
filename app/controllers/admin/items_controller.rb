b4aa3d32017a61e250d646e89140fb623788528e
Drew Conly <atconly@gmail.com>
2015-08-08 10:39:07 -0600

    added category

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,6 +8,8 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def create
+    category = Category.find_by(params[:category])
+    category_id = category.id
     @item = Item.new(item_params)
     if @item.save
       flash[:message] = "Item #{@item.title} created"
@@ -21,6 +23,6 @@ class Admin::ItemsController < Admin::BaseController
   private
 
   def item_params
-    params.require(:item).permit(:title, :price, :image, :description)
+    params.require(:item).permit(:title, :price, :image, :description, :category_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :category
+  validates :title, :description, :price, presence: true 
+
+  validates_uniqueness_of :title
 end
