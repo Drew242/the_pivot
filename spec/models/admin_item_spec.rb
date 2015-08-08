c732f473a47b73d4efd7c903b760e992fac419d5
Drew Conly <atconly@gmail.com>
2015-08-08 10:52:32 -0600

    added unit test

---
+++ spec/models/admin_item_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+RSpec.describe Item, type: :model do
+  context "with attributes" do
+    it "can be created" do
+      item = Item.new(title: "Parka",
+                      description: "Stay warm in the tundra",
+                      price: 3000,
+                      image: "https://s-media-cache-ak0.pinimg.com/236x/90/eb/32/90eb32bc73e010067b15e08cac3ff016.jpg")
+      item.save
+      expect(item).to be
+    end
+  end
+end
