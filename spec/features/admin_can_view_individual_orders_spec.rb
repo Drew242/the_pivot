1c7da67781788629e97a42d692b0d41474bcbece
mitchashby16 <mitchashby16@gmail.com>
2015-08-11 17:36:00 -0600

    generated all require content on orders show page for admin

--- spec/features/admin_can_view_individual_orders_spec.rb
+++ spec/features/admin_can_view_individual_orders_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.feature "AdminCanViewAnInvididualOrder", type: :feature do
   let!(:item) do
     Item.create(title: "Parka",
-                description: "Stay warm in the tundra",
-                price: 3000,
-                image: "https://s-media-cache-ak0.pinimg.com/236x/90/eb/32/90eb32bc73e010067b15e08cac3ff016.jpg")
+    description: "Stay warm in the tundra",
+    price: 3000,
+    image: "https://s-media-cache-ak0.pinimg.com/236x/90/eb/32/90eb32bc73e010067b15e08cac3ff016.jpg")
   end
   let!(:admin) { User.create(username: "admin", password: "password", role: 1) }
   let!(:user) {User.create(username: "Mitch", password: "supersecret", role: 0)}
@@ -25,9 +25,13 @@ RSpec.feature "AdminCanViewAnInvididualOrder", type: :feature do
       expect(page).to have_content("Created on #{order.created_at.strftime("%b %d, %Y")} at #{order.created_at.strftime("%I:%M:%S %p")}")
       expect(page).to have_content("Customer: Mitch")
       expect(page).to have_content("Shipping Address: 1510 Blake Street, Denver CO 80218")
-      # within ("#status_summary") do
-      #   expect(page).to have_content("Status: Ordered Count: 2")
-      # end
+      within("#admin_order_items") do
+        expect(page).to have_content("Parka")
+        expect(page).to have_content("Quantity: 2")
+        expect(page).to have_content("Price: $3,000.00")
+        expect(page).to have_content("Subtotal: $6,000.00")
+      end
+      expect(page).to have_content("Total: $6,000.00")
     end
   end
 end
