a77bb30779aa63c210dcc6c07848547445dfbaa3
Drew Conly <atconly@gmail.com>
2015-08-11 15:20:04 -0600

    changed to updated hash syntax

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
   has_many :orders, through: :order_items
   validates :title, presence: true
   validates :description, presence: true
-  validates :price, presence: true, :numericality => { :greater_than => 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 
   validates_uniqueness_of :title
   scope :top, -> { all.limit(10) }
