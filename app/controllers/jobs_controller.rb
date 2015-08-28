626ac6846507929ba09d6d05735591fedadb3dae
Drew Conly <atconly@gmail.com>
2015-08-28 15:18:52 -0600

    resolve fuzzy problem returning an array gem not working as predicted decide to remove or fix

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,7 @@ class JobsController < ApplicationController
     if params[:search_title]
       @jobs = Job.where(id: Job.find_by_fuzzy_title(params[:search_title]).map(&:id)).paginate(page: params[:page])
     end
-    
+
     if params[:search_location]
       @jobs = Job.where(id: Job.find_by_fuzzy_title(params[:search_location]).map(&:id)).paginate(page: params[:page])
     end
