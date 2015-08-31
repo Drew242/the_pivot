b30d0d1705b75364141018102a41eae453b7b0b5
Erik Butcher <with_a_k@mac.com>
2015-08-31 13:55:04 -0600

    Add more tests for search

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,12 +2,12 @@ class JobsController < ApplicationController
   def index
     @categories = Category.all
     @jobs = Job.paginate(page: params[:page])
-    if params[:search_title]
+    unless params[:search_title].empty?
       @jobs = Job.where(id: Job.find_by_fuzzy_title(params[:search_title]).map(&:id)).paginate(page: params[:page])
     end
 
-    if params[:search_location]
-      @jobs = Job.where(id: Job.find_by_fuzzy_title(params[:search_location]).map(&:id)).paginate(page: params[:page])
+    unless params[:search_location].empty?
+      @jobs = Job.where(id: Job.find_by_fuzzy_location(params[:search_location]).map(&:id)).paginate(page: params[:page])
     end
   end
 
