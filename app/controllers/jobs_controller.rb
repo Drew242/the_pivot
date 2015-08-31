23d69a09e5315cc63242794a28cf3d1ea070ad1f
Erik Butcher <with_a_k@mac.com>
2015-08-31 14:37:16 -0600

    Remove pagination from searches

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,16 +2,28 @@ class JobsController < ApplicationController
   def index
     @categories = Category.all
     @jobs = Job.paginate(page: params[:page])
-    unless params[:search_title].empty?
+    if searching_for_title?
       @jobs = Job.where(id: Job.find_by_fuzzy_title(params[:search_title]).map(&:id)).paginate(page: params[:page])
     end
 
-    unless params[:search_location].empty?
+    if searching_for_location?
       @jobs = Job.where(id: Job.find_by_fuzzy_location(params[:search_location]).map(&:id)).paginate(page: params[:page])
     end
   end
 
   def show
     @job = Job.find(params[:id])
+  end
+
+  private
+
+  def searching_for_title?
+    return false unless params[:search_title]
+    true unless params[:search_title].empty?
+  end
+
+  def searching_for_location?
+    return false unless params[:search_location]
+    true unless params[:search_location].empty?
   end
 end
