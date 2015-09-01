812ca3fd4c64abcf737d1530ecba70cedb9a7161
Rick Bacci <rickbacci@gmail.com>
2015-09-01 01:52:11 -0600

    add feature test for delete job

--- spec/features/store_admin_can_edit_and_delete_jobs_spec.rb
+++ spec/features/store_admin_can_edit_and_delete_jobs_spec.rb
@@ -49,4 +49,36 @@ RSpec.feature "Store Admin", type: :feature do
     expect(page).to have_content('edited dev description')
     expect(page).to have_content('Cleveland')
   end
+
+  it "can delete an existing job" do
+    Role.create(name: "registered_user")
+    Role.create(name: "company_admin")
+
+    click_link 'Employers'
+
+    fill_in 'Name', with: 'acme'
+    fill_in 'Information', with: 'acme info'
+
+    click_button 'Create Account'
+
+    expect(current_path).to eq(companies_admin_company_path(user.company.id))
+
+    click_link 'Create Job'
+
+    expect(current_path).to eq(new_companies_admin_job_path)
+
+    fill_in "Title", with: "dev"
+    fill_in "Description", with: "dev description"
+    fill_in "Location", with: "Denver"
+
+    click_button "Submit"
+
+    expect(current_path).to eq(companies_admin_company_path(user.company.id))
+
+    click_link 'Delete'
+
+    expect(page).to_not have_content('dev')
+    expect(page).to_not have_content('dev description')
+    expect(page).to_not have_content('Denver')
+  end
 end
