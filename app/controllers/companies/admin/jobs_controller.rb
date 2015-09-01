c3146416f1e06f95996b57c0be2c3864f8b03794
Rick Bacci <rickbacci@gmail.com>
2015-09-01 01:47:18 -0600

    edit job feature test passing

--- app/controllers/companies/admin/jobs_controller.rb
+++ app/controllers/companies/admin/jobs_controller.rb
@@ -27,6 +27,23 @@ class Companies::Admin::JobsController < Admin::BaseController
     end
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @company = Company.find_by(user_id: current_user.id)
+    @job = Job.find(params[:id])
+
+    if @job.update(job_params)
+      flash[:notice] = "Job updated!"
+      redirect_to companies_admin_company_path(@company)
+    else
+      flash[:error] = "Job update failed!"
+      render :edit
+    end
+  end
+
   def status
     @company = Company.find_by(user_id: current_user.id)
     @job = @company.jobs.find(params[:job])
