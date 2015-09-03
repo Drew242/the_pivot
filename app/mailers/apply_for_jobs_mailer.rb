569f96966ca78b6fa9172c24c5518271c77e2345
Rick Bacci <rickbacci@gmail.com>
2015-09-03 14:30:58 -0600

    mailers reaching sendgrid

---
+++ app/mailers/apply_for_jobs_mailer.rb
@@ -0,0 +1,28 @@
+class ApplyForJobsMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.apply_for_jobs_mailer.notify_user.subject
+  #
+  def notify_user(user)
+    @greeting = "Hi"
+    @user = user
+    mail to: user.email, subject: "You have successfully applied to your favorite jobs"
+
+
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.apply_for_jobs_mailer.notify_admin.subject
+  #
+  def notify_admin(user)
+    @greeting = "Hi"
+    @user = user
+    mail to: user.email, subject: "You have received an application for a job."
+  end
+end
+
+
