9a54d366307b35afee788e9b471ba3612488e997
Erik Butcher <with_a_k@mac.com>
2015-09-03 13:34:44 -0600

    Remove extra methods from jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,18 +8,4 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
-
-  private
-
-  def searching_for_title?
-    return false unless params[:search_title]
-    return true unless params[:search_title].empty?
-    false
-  end
-
-  def searching_for_location?
-    return false unless params[:search_location]
-    return true unless params[:search_location].empty?
-    false
-  end
 end
