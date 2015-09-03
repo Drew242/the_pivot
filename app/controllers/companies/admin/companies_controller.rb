cde9fee2e8de922f11450796e2f792c42247ae16
Erik Butcher <with_a_k@mac.com>
2015-09-03 14:38:26 -0600

    Improve coverage

--- app/controllers/companies/admin/companies_controller.rb
+++ app/controllers/companies/admin/companies_controller.rb
@@ -7,28 +7,4 @@ class Companies::Admin::CompaniesController < Admin::BaseController
   def show
     @company = Company.friendly.find(params[:id])
   end
-
-  def new
-    @company = Company.new
-  end
-
-  def create
-    @company = Company.new(company_params)
-    if current_user.company.nil? && @company.save
-      current_user.company = @company
-      current_user.user_roles.first.user_id = nil
-      current_user.roles << Role.find_by(name: "company_admin")
-      flash[:success] = "#{@company.name} has been created"
-      redirect_to root_path
-    else
-      flash.now[:danger] = "There was a problem creating #{@company.name}, you may only have one company."
-      render :new
-    end
-  end
-
-  private
-
-  def company_params
-    params.require(:company).permit(:name, :information)
-  end
 end
